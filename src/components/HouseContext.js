ae3e0ec67248defe859ae587fc6956cd8451ef68
Guilherme Parente <guilherme.parente17@outlook.com>
2024-10-15 16:37:08 -0300

    finish project

--- src/components/HouseContext.js
+++ src/components/HouseContext.js
@@ -1,7 +1,119 @@
-import React from 'react';
+import React, { useState, useEffect, createContext } from "react";
 
-const HouseContext = () => {
-  return <div>HouseContext</div>;
+import { housesData } from "../data";
+
+export const HouseContext = createContext();
+
+const HouseContextProvider = ({ children }) => {
+  const [houses, setHouses] = useState(housesData);
+  const [country, setCountry] = useState("Location (any)");
+  const [countries, setContries] = useState([]);
+  const [property, setProperty] = useState("Property type (any)");
+  const [properties, setProperties] = useState([]);
+  const [agents, setAgents] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [price, setPrice] = useState("Price range (any)");
+
+  useEffect(() => {
+    const allContries = houses.map((house) => house.country);
+    const uniqueContries = ["Location (any)", ...new Set(allContries)];
+    setContries(uniqueContries);
+  }, []);
+
+  useEffect(() => {
+    const allProperties = houses.map((house) => house.type);
+    const uniqueProperty = ["Location (any)", ...new Set(allProperties)];
+    setProperties(uniqueProperty);
+  }, []);
+
+  const handleClick = () => {
+    const isDefault = (str) => {
+      return str.split(" ").includes("(any)");
+    };
+
+    const minPrice = parseInt(price?.split(" ")[0]);
+    const maxPrice = parseInt(price?.split(" ")[2]);
+
+    const newHouses = housesData.filter((house) => {
+      setLoading(true);
+
+      const housePrice = parseInt(house.price);
+
+      if (
+        house.country === country &&
+        house.type === property &&
+        housePrice >= minPrice &&
+        housePrice <= maxPrice
+      ) {
+        return house;
+      }
+
+      if (isDefault(country) && isDefault(property) && isDefault(price)) {
+        return house;
+      }
+
+      if (!isDefault(country) && isDefault(property) && isDefault(price)) {
+        return house.country === country;
+      }
+
+      if (isDefault(country) && !isDefault(property) && isDefault(price)) {
+        return house.type === property;
+      }
+
+      if (isDefault(country) && isDefault(property) && !isDefault(price)) {
+        return housePrice >= minPrice && housePrice <= maxPrice;
+      }
+
+      if (!isDefault(country) && !isDefault(property) && isDefault(price)) {
+        return house.country === country && house.type === property;
+      }
+
+      if (!isDefault(country) && isDefault(property) && !isDefault(price)) {
+        if (housePrice >= minPrice && housePrice <= maxPrice) {
+          return house.country === country;
+        }
+      }
+
+      if (isDefault(country) && !isDefault(property) && !isDefault(price)) {
+        if (housePrice >= minPrice && housePrice <= maxPrice) {
+          return house.type === property;
+        }
+      }
+    });
+
+    setTimeout(() => {
+      return (
+        newHouses.length < 1 ? setHouses([]) : setHouses(newHouses),
+        setLoading(false)
+      );
+    }, 1000);
+  };
+
+  return (
+    <HouseContext.Provider
+      value={{
+        houses,
+        setHouses,
+        country,
+        setCountry,
+        countries,
+        setContries,
+        property,
+        setProperty,
+        properties,
+        setProperties,
+        agents,
+        setAgents,
+        loading,
+        price,
+        setPrice,
+        setLoading,
+        handleClick,
+      }}
+    >
+      {children}
+    </HouseContext.Provider>
+  );
 };
 
-export default HouseContext;
+export default HouseContextProvider;
